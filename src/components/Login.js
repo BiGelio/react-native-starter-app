204d3e9f13e4242b103e6a9bb6572bb8058d2664
bigelio <geliobizimana01@gmail.com>
2021-04-03 12:11:32 +0200

    Creating login component

---
+++ src/components/Login.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+export default class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+  handleEmail = text => {
+    this.setState({email: text});
+  };
+  handlePassword = text => {
+    this.setState({password: text});
+  };
+  login = (email, password) => {
+    alert('email:' + email + 'password:' + password);
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          underlineColorAndroid="transparent"
+          placeholderTextColor="black"
+          autoCapitalize="none"
+          onChangeText={this.handleEmail}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          underlineColorAndroid="transparent"
+          placeholderTextColor="black"
+          autoCapitalize="none"
+          onChangeText={this.handlePassword}
+        />
+        <TouchableOpacity
+          style={styles.submitBtn}
+          onPress={() => this.login(this.state.email, this.state.password)}>
+          <Text style={styles.submitBtnText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 23,
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: '#7a42f4',
+    borderWidth: 1,
+  },
+  submitBtn: {
+    backgroundColor: '#7a42f4',
+    padding: 10,
+    margin: 15,
+    height: 40,
+  },
+  submitBtnText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
